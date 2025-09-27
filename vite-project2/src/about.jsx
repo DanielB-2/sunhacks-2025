2249dce977d844b76c7fc081ef2ef1724acfd7cc
DanielB-2 <dtbriggs123@gmail.com>
2025-09-27 16:54:55 -0700

    basic about page added to .jsx format

--- vite-project2/src/about.jsx
+++ vite-project2/src/about.jsx
@@ -13,11 +13,24 @@ function App() {
       <div>
         <TopBar />
       </div>
-      <div>
-        
-       
-      </div>
-      
+      <div class="flex flex-row h-80 flex-wrap">
+        <div class="min-w-100 rounded-4x1 flex-1 text-center content-around px-32">
+            <h1 class="text-4xl">What is Recyclix</h1>
+        </div>
+        <div class="text-wrap min-w-100 rounded-4x1 flex-1 text-left content-around px-32">
+            <p class="text-l">Recyclix is a website that encourages users to recycle. By giving user's points for recycling correctly they can compete angainst friends.</p>
+        </div>
+    </div>
+
+    <div class="flex flex-row h-80 flex-wrap">
+        <div class="min-w-100 rounded-4x1 flex-1 text-center content-around px-32">
+            <p class="text-l">Recyclix was made by Anirudh Gali, Pratyush Grover, and Daniel Briggs for sunhacks 2025 at ASU</p>
+        </div>
+        <div class="text-wrap min-w-100 rounded-4x1 flex-1 text-left content-around px-32">
+            
+            <h1 class="text-4xl">Who Made Recyclix</h1>
+        </div>
+    </div>
     </div>
   )
 }
