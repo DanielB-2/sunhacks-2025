c4ec7c4e7e70f33e39748c46d1732ff448993f63
DanielB-2 <dtbriggs123@gmail.com>
2025-09-27 16:53:45 -0700

    added basic about page

--- vite-project2/src/about.jsx
+++ vite-project2/src/about.jsx
@@ -10,21 +10,13 @@ function App() {
 
   return (
     <div>
-      <div class="flex flex-row h-200">
-        <div class="rounded-4x1 flex-1 text-center content-around">
-            <h1>What is Recyclix</h1>
-        </div>
-        <div class="rounded-4x1 flex-1 text-left">
-            <p>Recyclix is a website that encourages users to recycle. By giving user's points for recycling correctly they can compete angainst friends.</p>
-        </div>
-    </div>
       <div>
         <TopBar />
       </div>
       <div>
-        <MainPage></MainPage>
+        
+       
       </div>
-      
       
     </div>
   )
