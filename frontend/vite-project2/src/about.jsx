27546df23feafa5c1b5e5a9f881b9cae5a913d48
DanielB-2 <dtbriggs123@gmail.com>
2025-09-27 21:46:13 -0700

    login and signup page converted to jsx and some formatting changes, about page formatting and spelling changes

--- frontend/vite-project2/src/about.jsx
+++ frontend/vite-project2/src/about.jsx
@@ -1,19 +1,18 @@
 
-import { TopBar } from "./Components/TopBar";
 
 export function About() {
   return (
-    <div>
+    <div className = "min-h-[100vh] pb-[72px] bg-[#f9f4eb]">
       
 
       
       <div className="flex flex-row h-80 flex-wrap">
         <div className="min-w-100 rounded-xl flex-1 text-center px-[5vw] flex items-center justify-center">
-          <h1 className="text-4xl font-bold">What is Recyclix</h1>
+          <h1 className="text-4xl font-bold">What is RecycleX</h1>
         </div>
         <div className="min-w-100 rounded-xl flex-1 text-left px-[5vw] flex items-center">
           <p className="text-lg">
-            Recyclix is a website that encourages users to recycle. By giving
+            RecycleX is a website that encourages users to recycle. By giving
             users points for recycling correctly they can compete against
             friends.
           </p>
@@ -24,12 +23,12 @@ export function About() {
       <div className="flex flex-row h-80 flex-wrap-reverse">
         <div className="min-w-100 rounded-xl flex-1 text-left px-[5vw] flex items-center">
           <p className="text-lg">
-            Recyclix was made by <b>Anirudh Gali</b>, <b>Pratyush Grover</b>, and
-            <b> Daniel Briggs</b> for SunHacks 2025 at ASU.
+            RecycleX was made by <b>Anirudh Gali</b>, <b>Pratyush Grover</b>, and
+            <b> Daniel Briggs</b> for sunhacks 2025 at ASU.
           </p>
         </div>
         <div className="min-w-100 rounded-xl flex-1 text-center px-[5vw] flex items-center justify-center">
-          <h1 className="text-4xl font-bold">Who Made Recyclix</h1>
+          <h1 className="text-4xl font-bold">Who Made RecycleX</h1>
         </div>
       </div>
     </div>
